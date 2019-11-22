8487e55dcd2d671aeb6840ffd92ea6293d066df3
Mohit Aggarwal <mohit.aggarwal@arm.com>
2019-11-22 12:55:15 -0600

    cosmetic changes and added support for passing list/range of floats by removing the int conversion in mapping_int in domain space

--- mango/test/test_mango.py
+++ mango/test/test_mango.py
@@ -13,11 +13,12 @@ from scipy.stats import uniform
 from mango.tuner import Tuner
 
 # Simple param_dict
-param_dict = {"a": uniform(0, 1), # uniform distribution
-              "b": range(1,5), # Integer variable
-              "c":[1,2], # Integer variable
-              "d":["-1","1"] # Categorical variable
-             }
+param_dict = {"a": uniform(0, 1),  # uniform distribution
+              "b": range(1, 5),  # Integer variable
+              "c": [1, 2],  # Integer variable
+              "d": ["-1", "1"]  # Categorical variable
+              }
+
 
 # Simple objective function
 def objectiveFunction(args_list):
@@ -31,49 +32,53 @@ def objectiveFunction(args_list):
         results.append(result)
     return results
 
+
 domain_size = 10
 
-#test the functionality of domain space transformations
-def test_domain():
-    ds = domain_space(param_dict,domain_size)
 
-    #getting the samples from the domain
+# test the functionality of domain space transformations
+def test_domain():
+    ds = domain_space(param_dict, domain_size)
+
+    # getting the samples from the domain
     domain_list = ds.get_domain()
 
-    #number of samples should have same size as the domain_size
+    # number of samples should have same size as the domain_size
     assert len(domain_list) == domain_size
 
-    #change this into the GP domain space
+    # change this into the GP domain space
     domain_np = ds.convert_GP_space(domain_list)
 
-    #in gaussian space verifying correctness of samples sizes and structure
+    # in gaussian space verifying correctness of samples sizes and structure
     assert domain_np.shape[0] == domain_size
 
-    #test the reverse conversion
+    # test the reverse conversion
     domain_ps = ds.convert_PS_space(domain_np)
 
-    #domain_ps and domain_list should be same
+    # domain_ps and domain_list should be same
     assert type(domain_list) == type(domain_ps)
     assert len(domain_list) == len(domain_ps)
 
-    #testing samples should be from param_dict and structure is preserved in transformations
-    if len(domain_list)>0:
+    # testing samples should be from param_dict and structure is preserved in transformations
+    if len(domain_list) > 0:
         l1 = domain_list[0]
         l2 = domain_ps[0]
         assert type(l1) == type(l2)
         assert len(l1.keys()) == len(l2.keys())
-        #all keys should be drawn from the param_dict
+        # all keys should be drawn from the param_dict
         assert len(l1.keys()) == len(param_dict.keys())
 
         for key in l1.keys():
             assert key in param_dict.keys()
 
-#test the functionality of the tuner
+
+# test the functionality of the tuner
 def test_tuner():
     tuner_user = Tuner(param_dict, objectiveFunction)
     results = tuner_user.run()
-    #max objective is 8, and minimum is 1
-    assert results['best_objective'] >1
+    # max objective is 8, and minimum is 1
+    assert results['best_objective'] > 1
+
 
 # test on Rosenbrock's Valley
 # Rosenbrock's valley (a.k.k the banana function) has a global optimimum lying inside a long, narrow parabolic valley with a flat floor
@@ -119,12 +124,13 @@ def test_convex():
 
     x_opt = 0
     y_opt = 0
+
     def objfunc(args_list):
         results = []
         for hyper_par in args_list:
             x = hyper_par['x']
             y = hyper_par['y']
-            result = -(x**2 + y**2)
+            result = -(x ** 2 + y ** 2)
             results.append(result)
         return results
 
@@ -138,12 +144,11 @@ def test_convex():
     tuner = Tuner(param_dict, objfunc)
     results = tuner.run()
 
-    print('best hyper parameters:',results['best_hyper_parameter'])
-    print('best Accuracy:',results['best_objective'])
+    print('best hyper parameters:', results['best_params'])
+    print('best Accuracy:', results['best_objective'])
 
-    assert abs(results['best_hyper_parameter']['x'] - x_opt) <= 3
-    assert abs(results['best_hyper_parameter']['x'] - y_opt) <= 3
-
+    assert abs(results['best_params']['x'] - x_opt) <= 3
+    assert abs(results['best_params']['x'] - y_opt) <= 3
 
 # def test_six_hump():
 #     def camel(x,y):
