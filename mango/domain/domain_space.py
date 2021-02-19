added batch sampler, removed scaling from domain sampling, reverted changes to GP regressor, fixed alpha and other changes base don kdd benchmark learnings

mango/domain/domain_space.py
@@ -6,9 +6,11 @@ Define the domain space abstractions for the Optimizer
 # if the parameter type is a disribution or rv_frozen, the parameter sampler can handle it
 from scipy.stats._distn_infrastructure import rv_frozen
 import numpy as np
-from sklearn.model_selection import ParameterSampler
 from sklearn.preprocessing import StandardScaler
 from collections.abc import Iterable
+
+from .batch_parameter_sampler import BatchParameterSampler
+
 
 class domain_space():
     """
@@ -20,8 +22,7 @@ class domain_space():
 
     def __init__(self,
                  param_dict,
-                 domain_size,
-                 scaled=False):
+                 domain_size):
         self.param_dict = param_dict
 
         # the domain size to explore using the parameter sampler
@@ -30,22 +31,6 @@ class domain_space():
         # creating a mapping of categorical variables
         self.create_mappings()
 
-        # create scaler for GP features
-        # only used with old sampling methods
-        if scaled:
-            self.scaler = self.create_scaler()
-
-    def create_scaler(self):
-        """
-        create scaler for features that go into GPR
-        using a fixed set of initial domain samples
-        """
-        domain_list = self.get_domain()
-        x_gp = self.convert_GP_space(domain_list)
-        scaler = StandardScaler()
-        scaler.fit(x_gp)
-        return scaler
-
 
     """
     returns the list of domain values using the parameter sampler
@@ -53,7 +38,7 @@ class domain_space():
     """
 
     def get_domain(self):
-        domain_list = list(ParameterSampler(self.param_dict, n_iter=self.domain_size))
+        domain_list = list(BatchParameterSampler(self.param_dict, n_iter=self.domain_size))
         return domain_list
 
     """
@@ -61,7 +46,7 @@ class domain_space():
     """
 
     def get_random_sample(self, size):
-        domain_list = list(ParameterSampler(self.param_dict, n_iter=size))
+        domain_list = list(BatchParameterSampler(self.param_dict, n_iter=size))
         return domain_list
 
     """
@@ -77,18 +62,15 @@ class domain_space():
     def create_mappings(self):
         mapping_categorical = dict()
         mapping_int = dict()
-        intervals = dict()
 
         param_dict = self.param_dict
         for par in param_dict:
             if isinstance(param_dict[par], rv_frozen):
                 # FIXME: what if the distribution generators ints , GP would convert it to float
-                intervals[par] = param_dict[par].interval(1.)
                 pass  # we are not doing anything at present, and will directly use its value for GP.
 
             elif isinstance(param_dict[par], range):
                 mapping_int[par] = param_dict[par]
-                intervals[par] = (min(param_dict[par]), max(param_dict[par]))
 
             elif isinstance(param_dict[par], Iterable):
 
@@ -102,154 +84,14 @@ class domain_space():
 
                 if all_int:
                     mapping_int[par] = param_dict[par]
-                    intervals[par] = (min(param_dict[par]), max(param_dict[par]))
 
                 # For lists with mixed type, floats or strings we consider them categorical or discrete
                 else:
                     mapping_categorical[par] = param_dict[par]
-                    intervals[par] = (0, 1)
 
         self.mapping_categorical = mapping_categorical
         self.mapping_int = mapping_int
-        self.intervals = intervals
 
-    @property
-    def gp_features_count(self):
-        m = 0
-        for param in self.param_dict:
-            if param in self.mapping_categorical:
-                m += len(self.mapping_categorical[param])
-            else:
-                m += 1
-
-        return m
-
-    @property
-    def param_gp_index(self):
-        """
-        Returns dict mapping parameter name to gp feature index
-        """
-        res = {}
-        idx = 0
-        for param in sorted(self.param_dict.keys()):
-            if param not in self.mapping_categorical:
-                res[param] = idx
-                idx += 1
-            else:
-                res[param] = idx
-                idx += len(self.mapping_categorical[param])
-
-        return res
-
-    def sample_gp_space(self, n_samples=None):
-        """
-        Generate sample in the Gaussian process space
-
-        Returns a n_samples x m numpy array (m is the number of GP features)
-        """
-        if n_samples is None:
-            n_samples = self.domain_size
-
-        m = self.gp_features_count
-        idx_map = self.param_gp_index
-        X = np.zeros((n_samples, m))
-        for param in self.param_dict:
-            distribution = self.param_dict[param]
-            if param not in self.mapping_categorical:
-                if isinstance(distribution, rv_frozen):
-                    X[:, idx_map[param]] = np.random.uniform(size=n_samples)
-                else:
-                    random_samples = np.random.choice(distribution, size=n_samples)
-                    _min = self.intervals[param][0]
-                    _max = self.intervals[param][1]
-                    _scale = _max - _min
-                    if _max == _min:
-                        _scale = 1.
-                    X[:, idx_map[param]] = (random_samples - _min) / _scale
-            else:
-                n_classes = len(self.mapping_categorical[param])
-                random_one_hot = np.eye(n_classes)[np.random.choice(n_classes, n_samples)]
-                X[:, idx_map[param]:idx_map[param] + n_classes] = random_one_hot
-
-        return X
-
-    def convert_to_params(self, X):
-        """
-        Convert GP feature samples to Parameter dictionaries (inverse of sample_gp_space method)
-        Params:
-        X: n_samples x m_gp_features numpy array
-
-        Returns list of dictionaries with param names as keys
-        """
-        res = {}
-        idx_map = self.param_gp_index
-        for param in self.param_dict:
-            gp_index = idx_map[param]
-            distribution = self.param_dict[param]
-            if param not in self.mapping_categorical:
-                if isinstance(distribution, rv_frozen):
-                    val = distribution.ppf(X[:, gp_index])
-                else:
-                    _min = self.intervals[param][0]
-                    _max = self.intervals[param][1]
-                    _scale = _max - _min
-                    if _max == _min:
-                        _scale = 1.
-                    val = (X[:, gp_index] * _scale) + _min
-
-                if param in self.mapping_int:
-                    val = [int(v) for v in np.rint(val)]
-
-                res[param] = val
-            else:
-                n_classes = len(self.mapping_categorical[param])
-                one_hot_samples = X[:, gp_index:gp_index + n_classes]
-                res[param] = [distribution[idx] for idx in np.argmax(one_hot_samples, axis=1)]
-
-        # convert dict of lists to list of dicts
-        result = []
-        for idx in range(X.shape[0]):
-            result.append({param: value[idx] for param, value in res.items()})
-
-        return result
-
-    def convert_to_gp(self, params_list):
-        """
-        Convert parameters to GP features
-
-        params_list: list of dicts with parameter name as keys
-
-        Returns 2D array n_samples x m_gp_features
-        """
-        n_samples = len(params_list)
-        m = self.gp_features_count
-        X = np.zeros((n_samples, m))
-        idx_map = self.param_gp_index
-
-        for param in self.param_dict:
-            gp_index = idx_map[param]
-            values = [d[param] for d in params_list]
-            distribution = self.param_dict[param]
-            if param not in self.mapping_categorical:
-                values = np.array(values)
-                if isinstance(distribution, rv_frozen):
-                    X[:, gp_index] = distribution.cdf(values)
-                else:
-                    _min = self.intervals[param][0]
-                    _max = self.intervals[param][1]
-                    _scale = _max - _min
-                    if _max == _min:
-                        _scale = 1.
-                    X[:, gp_index] = (values - _min) / _scale
-
-            else:
-                n_classes = len(self.mapping_categorical[param])
-                one_hot = np.zeros((n_samples, n_classes))
-                for row, v in enumerate(values):
-                    one_hot[row, distribution.index(v)] = 1
-                X[:, gp_index:gp_index + n_classes] = one_hot
-
-        return X
 
     """
     convert the hyperparameters from the param_dict space to the GP space, by converting the
@@ -289,8 +131,6 @@ class domain_space():
             X.append(curr_x)
 
         X = np.array(X)
-        if hasattr(self, 'scaler'):
-            X = self.scaler.transform(X)
 
         return X
 
@@ -303,9 +143,6 @@ class domain_space():
 
     def convert_PS_space(self, X_gp):
         X_ps = []
-
-        if hasattr(self, 'scaler'):
-            X_gp = self.scaler.inverse_transform(X_gp)
 
         mapping_categorical = self.mapping_categorical
         mapping_int = self.mapping_int

mango/tuner.py
@@ -4,6 +4,7 @@ General usage is to find the optimal hyper-parameters of the classifier
 """
 
 from dataclasses import dataclass
+import random
 
 from mango.domain.domain_space import domain_space
 from mango.optimizer.bayesian_learning import BayesianLearning
@@ -12,33 +13,36 @@ from scipy.stats._distn_infrastructure import rv_frozen
 from tqdm.auto import tqdm
 import numpy as np
 
-## setting warnings to ignore for now
+# setting warnings to ignore for now
 import warnings
 
 warnings.filterwarnings('ignore')
 
 
 class Tuner:
-
     @dataclass
     class Config:
         domain_size: int = None
-        initial_random: int = 1
+        initial_random: int = 2
         num_iteration: int = 20
         batch_size: int = 1
         optimizer: str = 'Bayesian'
         parallel_strategy: str = 'clustering'
-        surrogate: object = None # used to test different kernel functions
-        scale_y: bool = False
-
+        surrogate: object = None  # used to test different kernel functions
         valid_optimizers = ['Bayesian', 'Random']
         valid_parallel_strategies = ['penalty', 'clustering']
+        alpha: float = 2.0
+        exploration: float = 1.0
+        exploration_decay: float = 0.9
+        exploration_min: float = 0.1
+        fixed_domain: bool = False
 
         def __post_init__(self):
             if self.optimizer not in self.valid_optimizers:
-                raise ValueError(f'optimizer: {self.optimizer} is not valid, should be one of {self.valid_optmizers}')
+                raise ValueError(f'optimizer: {self.optimizer} is not valid, should be one of {self.valid_optimizers}')
             if self.parallel_strategy not in self.valid_parallel_strategies:
-                raise ValueError(f'parallel strategy: {self.parallel_strategy} is not valid, should be one of {self.valid_parallel_strategies}')
+                raise ValueError(
+                    f'parallel strategy: {self.parallel_strategy} is not valid, should be one of {self.valid_parallel_strategies}')
 
         @property
         def is_bayesian(self):
@@ -74,6 +78,9 @@ class Tuner:
         if hasattr(objective, 'batch_size'):
             self.config.batch_size = objective.batch_size
 
+        # save domain size
+        self.ds = domain_space(self.param_dict, self.config.domain_size)
+
         # stores the results of using the tuner
         self.results = dict()
 
@@ -84,7 +91,7 @@ class Tuner:
            optimum of bayesian optimizer
         """
         # Minimum and maximum domain size
-        domain_min = 5000
+        domain_min = 50000
         domain_max = 500000
 
         domain_size = 1
@@ -134,17 +141,14 @@ class Tuner:
     def runBayesianOptimizer(self):
         results = dict()
 
-        # domain space abstraction
-        ds = domain_space(self.param_dict, self.config.domain_size)
-
         # getting first few random values
-        random_hyper_parameters = ds.get_random_sample(self.config.initial_random)
+        random_hyper_parameters = self.ds.get_random_sample(self.config.initial_random)
         X_list, Y_list = self.runUserObjective(random_hyper_parameters)
 
         # in case initial random results are invalid try different samples
         n_tries = 1
         while len(Y_list) < self.config.initial_random and n_tries < 3:
-            random_hps = ds.get_random_sample(self.config.initial_random - len(Y_list))
+            random_hps = self.ds.get_random_sample(self.config.initial_random - len(Y_list))
             X_list2, Y_list2 = self.runUserObjective(random_hps)
             random_hyper_parameters.extend(random_hps)
             X_list = np.append(X_list, X_list2)
@@ -155,15 +159,16 @@ class Tuner:
             raise ValueError("No valid configuration found to initiate the Bayesian Optimizer")
 
         # evaluated hyper parameters are used
-        X_init = ds.convert_to_gp(X_list)
+        X_init = self.ds.convert_GP_space(X_list)
         Y_init = Y_list.reshape(len(Y_list), 1)
 
         # setting the initial random hyper parameters tried
         results['random_params'] = X_list
         results['random_params_objective'] = Y_list
 
-        Optimizer = BayesianLearning(surrogate=self.config.surrogate, n_features=X_init.shape[1])
-        Optimizer.domain_size = self.config.domain_size
+        Optimizer = BayesianLearning(surrogate=self.config.surrogate,
+                                     alpha=self.config.alpha,
+                                     domain_size=self.config.domain_size)
 
         X_sample = X_init
         Y_sample = Y_init
@@ -174,42 +179,40 @@ class Tuner:
 
         x_failed_evaluations = np.array([])
 
+        domain_list = self.ds.get_domain()
+        X_domain_np = self.ds.convert_GP_space(domain_list)
+
         # running the iterations
         pbar = tqdm(range(self.config.num_iteration))
         for i in pbar:
-            # Domain Space
-            X_domain_np = ds.sample_gp_space()
+            # adding a Minimum exploration to explore independent of UCB
+            if random.random() < self.config.exploration:
+                random_parameters = self.ds.get_random_sample(self.config.batch_size)
+                X_next_batch = self.ds.convert_GP_space(random_parameters)
 
-            # Black-Box Optimizer
-            if self.config.scale_y:
-                scale = np.max(Y_sample) - np.min(Y_sample)
-                if scale == 0:
-                    scale = np.max(np.abs(Y_sample))
-                Y_scaled = (Y_sample - np.mean(Y_sample)) / scale
-            else:
-                Y_scaled = Y_sample
+                if self.config.exploration > self.config.exploration_min:
+                    self.config.exploration = self.config.exploration * self.config.exploration_decay
 
-            if self.config.strategy_is_penalty:
-                X_next_batch = Optimizer.get_next_batch(X_sample, Y_scaled, X_domain_np,
-                                                    batch_size=self.config.batch_size)
+            elif self.config.strategy_is_penalty:
+                X_next_batch = Optimizer.get_next_batch(X_sample, Y_sample, X_domain_np,
+                                                        batch_size=self.config.batch_size)
             elif self.config.strategy_is_clustering:
-                X_next_batch = Optimizer.get_next_batch_clustering(X_sample,Y_scaled, X_domain_np,
+                X_next_batch = Optimizer.get_next_batch_clustering(X_sample, Y_sample, X_domain_np,
                                                                    batch_size=self.config.batch_size)
             else:
                 # assume penalty approach
-                X_next_batch = Optimizer.get_next_batch(X_sample, Y_scaled, X_domain_np,
+                X_next_batch = Optimizer.get_next_batch(X_sample, Y_sample, X_domain_np,
                                                         batch_size=self.config.batch_size)
 
             # Scheduler
-            X_next_PS = ds.convert_to_params(X_next_batch)
+            X_next_PS = self.ds.convert_PS_space(X_next_batch)
 
             # if all the xs have failed before, replace them with random sample
             # as we will not get any new information otherwise
             if all(x in x_failed_evaluations for x in X_next_PS):
-                X_next_PS = ds.get_random_sample(self.config.batch_size)
+                X_next_PS = self.ds.get_random_sample(self.config.batch_size)
 
             # Evaluate the Objective function
-            # Y_next_batch, Y_next_list = self.runUserObjective(X_next_PS)
             X_next_list, Y_next_list = self.runUserObjective(X_next_PS)
 
             # keep track of all parameters that failed
@@ -223,7 +226,7 @@ class Tuner:
 
             Y_next_batch = Y_next_list.reshape(len(Y_next_list), 1)
             # update X_next_batch to successfully evaluated values
-            X_next_batch = ds.convert_to_gp(X_next_list)
+            X_next_batch = self.ds.convert_GP_space(X_next_list)
 
             # update the bookeeping of values tried
             hyper_parameters_tried = np.append(hyper_parameters_tried, X_next_list)
@@ -233,6 +236,12 @@ class Tuner:
             # Appending to the current samples
             X_sample = np.vstack((X_sample, X_next_batch))
             Y_sample = np.vstack((Y_sample, Y_next_batch))
+
+            # referesh domain if not fixed
+            if not self.config.fixed_domain:
+                domain_list = self.ds.get_domain()
+                X_domain_np = self.ds.convert_GP_space(domain_list)
+
             pbar.set_description("Best score: %s" % np.max(Y_sample))
 
         results['params_tried'] = hyper_parameters_tried
@@ -248,40 +257,43 @@ class Tuner:
 
         # saving the optimizer and ds in the tuner object which can save the surrogate function and ds details
         self.Optimizer = Optimizer
-        self.ds = ds
+
         return results
 
     def runRandomOptimizer(self):
         results = dict()
-        # domain space abstraction
-        ds = domain_space(self.param_dict, self.config.domain_size)
 
         X_sample_list = []
         Y_sample_list = []
 
+        batch_size = self.config.batch_size
+        n_iterations = self.config.num_iteration
+        random_hyper_parameters = self.ds.get_random_sample(n_iterations * batch_size)
+
         # running the iterations
-        pbar = tqdm(range(self.config.num_iteration))
-        for i in pbar:
+        pbar = tqdm(range(0, len(random_hyper_parameters), batch_size))
+        for idx in pbar:
             # getting batch by batch random values to try
-            random_hyper_parameters = ds.get_random_sample(self.config.batch_size)
-            X_list, Y_list = self.runUserObjective(random_hyper_parameters)
+            batch_hyper_parameters = random_hyper_parameters[idx:idx + batch_size]
+            X_list, Y_list = self.runUserObjective(batch_hyper_parameters)
 
             X_sample_list = np.append(X_sample_list, X_list)
             Y_sample_list = np.append(Y_sample_list, Y_list)
 
-            pbar.set_description("Best score: %s" % np.max(np.array(Y_sample_list)))
+            # FIXME: account for when maximizing as it would print neg values
+            pbar.set_description("Best score: %s" % np.max(Y_sample_list))
 
         # After all the iterations are done now bookkeeping and best hyper parameter values
         results['params_tried'] = X_sample_list
         results['objective_values'] = Y_sample_list
 
         if len(Y_sample_list) > 0:
-            results['best_objective'] = np.max(Y_sample_list)
-            results['best_params'] = X_sample_list[np.argmax(Y_sample_list)]
+            results['best_objective'] = np.max(results['objective_values'])
+            results['best_params'] = results['params_tried'][np.argmax(results['objective_values'])]
 
-        if self.maximize_objective is False:
-            results['objective_values'] = -1 * results['objective_values']
-            results['best_objective'] = -1 * results['best_objective']
+            if self.maximize_objective is False:
+                results['objective_values'] = -1 * results['objective_values']
+                results['best_objective'] = -1 * results['best_objective']
 
         return results
 

tests/test_domain_space.py
@@ -56,11 +56,11 @@ def test_gp_samples_to_params():
     }
     X = np.array([
         # 4, -8, 'cat2', 1, 'const', 1 , 10
-        [0.4444, 0.1, 0, 0, 1, 0, 1, 0.6, 0],
+        [4, -8, 0, 0, 1, 1, 1, 1, 10],
         # 0, -10.0, 'cat1', 3, 'const', 0.001 , 10
-        [0.0, 0.0, 1, 0, 0, 1, 1, 0.0, 0],
+        [0, -10, 1, 0, 0, 3, 1, 0.001, 10],
         # 9, 10.0, 1, 2, 'const', 100 , 10
-        [1.0, 1.0, 0, 1, 0, 0.5, 1, 1.0, 0],
+        [9, 10.0, 0, 1, 0, 2, 1, 100.0, 10],
     ])
 
     expected = [
@@ -71,7 +71,7 @@ def test_gp_samples_to_params():
 
     ds = domain_space(space, domain_size=1000)
 
-    params = ds.convert_to_params(X)
+    params = ds.convert_PS_space(X)
 
     for act, exp in zip(params, expected):
         for k, v in act.items():
@@ -95,17 +95,16 @@ def test_gp_space():
     }
 
     ds = domain_space(space, domain_size=10000)
-    X = ds.sample_gp_space()
+    domain_list = ds.get_domain()
+    X = ds.convert_GP_space(domain_list)
 
-    assert (X <= 1.0).all()
-    assert (X >= 0.0).all()
     assert (X[:, 0] == 1.).all()  # a
-    assert (X[:, 1] == 0.).all()  # b
-    assert np.isin(X[:, 2], [0.0, 0.5, 1.0]).all()  # c
+    assert (X[:, 1] == 10.).all()  # b
+    assert np.isin(X[:, 2], [1, 2, 3]).all()  # c
     assert np.isin(X[:, 4:7], np.eye(3)).all()  # e
     assert X.shape == (ds.domain_size, 12)
 
-    params = ds.convert_to_params(X)
+    params = ds.convert_PS_space(X)
 
     for param in params:
         assert param['a'] == 'const'
@@ -118,5 +117,5 @@ def test_gp_space():
         assert -10 <= param['h'] <= 10
         assert param['i'] in space['i']
 
-    X2 = ds.convert_to_gp(params)
+    X2 = ds.convert_GP_space(params)
     assert np.isclose(X2, X).all()